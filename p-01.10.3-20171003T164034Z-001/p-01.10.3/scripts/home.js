3176357efb23bd4ed68b285719c535c4f965f2a2
Karl <karlxu0410@gmail.com>
2017-10-08 16:57:34 -0700

    10/8-2
    same

--- p-01.10.3-20171003T164034Z-001/p-01.10.3/scripts/home.js
+++ p-01.10.3-20171003T164034Z-001/p-01.10.3/scripts/home.js
@@ -3,20 +3,31 @@ var tab=document.getElementById("demo");
 var tab1=document.getElementById("demo1");
 var tab2=document.getElementById("demo2");
 tab2.innerHTML=tab1.innerHTML;
-function Marquee(){
-tab.scrollLeft++;
-}
+var counter = 0;
 
-function stop(){
+function Marquee(){
+  if(tab2.offsetWidth-tab.scrollLeft<=0)
+    {tab.scrollLeft-=tab1.offsetWidth;}
     
+else{tab.scrollLeft++;
+   
+    }
+
 }
 
 var MyMar=setInterval(Marquee,speed);
+setTimeout(function(){clearInterval(MyMar)};,200000);
+//setTimeout(clearInterval(MyMar),3000);
+
+
+//setTimeout(Marquee(),5000);
+//for (i = 1;i<=100;++i){
+  //  setDelay(i);
+//}
+
+//function setDelay(i){
+//  setTimeout(Marquee,i);
+//}
+
+//var MyMar=setInterval(Marquee,speed);
 //var my_stop = 
-tab.onmouseover=function() {clearInterval(MyMar)};
-tab.onmouseout=function() {
-    
-    
-    
-    
-    MyMar=setInterval(Marquee,speed)};
